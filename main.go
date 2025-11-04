71af00803ab3a1b5051f033f9bf213b4ca8bfbe8
Aman Mangal <mangalaman93@gmail.com>
2025-11-05 01:19:08 +0530

    handle interrupt signal and NO screenshot at night

--- main.go
+++ main.go
@@ -12,8 +12,8 @@ import (
 )
 
 const (
-	screenshotFolder = "ss"
-	mapsURL          = "https://www.google.com/maps/@26.9170646,75.8158598,600m/data=!3m1!1e3!5m1!1e1"
+	screenshotFolder  = "ss"
+	mapsURLForYadgaar = "https://www.google.com/maps/@26.9170646,75.8158598,600m/data=!3m1!1e3!5m1!1e1"
 )
 
 var (
@@ -28,30 +28,45 @@ var (
 
 func main() {
 	// Register signal handler
-	signalChan := make(chan os.Signal, 1)
-	signal.Notify(signalChan, os.Interrupt)
+	ctrlC := make(chan os.Signal, 1)
+	signal.Notify(ctrlC, os.Interrupt)
+	defer signal.Stop(ctrlC)
 
-	t := time.NewTicker(5 * time.Minute)
+	t := time.NewTicker(10 * time.Minute)
+	defer t.Stop()
+
 	for {
 		select {
 		case <-t.C:
-			if err := takeScreenshot(); err != nil {
-				log.Print(err)
+			// no screenshot during 23:30 to 6:30 IST, that is 18 to 1 UTC
+			if time.Now().UTC().Hour() >= 18 || time.Now().UTC().Hour() <= 0 {
+				log.Println("skipping screenshot during 23:30 to 6:30 IST")
+				continue
 			}
 
-		case <-signalChan:
-			log.Println("Shutting down...")
+			// Take screenshot for Yadgaar junction
+			if err := takeScreenshotForYadgaar(); err != nil {
+				log.Println(err)
+			}
+
+		case <-ctrlC:
+			log.Println("shutting down...")
 			return
 		}
 	}
 }
 
-func takeScreenshot() error {
+func takeScreenshotForYadgaar() error {
+	defer func() {
+		if r := recover(); r != nil {
+			log.Printf("error: recovered panic: %v\n", r)
+		}
+	}()
+
 	h := orcgen.NewHandler(orcgen.ScreenshotConfig{Clip: &gViewportConfig})
-	pngPass, err := orcgen.ConvertWebpage(h, mapsURL)
+	pngPass, err := orcgen.ConvertWebpage(h, mapsURLForYadgaar)
 	if err != nil {
 		return fmt.Errorf("error while loading the webpage: %w", err)
-
 	}
 
 	fileName := fmt.Sprintf("%v/yadgaar_%v.png", screenshotFolder, time.Now().Format("20060102-150405"))
